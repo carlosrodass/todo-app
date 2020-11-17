93490d8c2b06349d97d9a432f52cd29e6701da81
Carlos <crodas035@gmail.com>
2020-11-17 16:13:08 +0100

    localStorage a medias

--- index.js
+++ index.js
@@ -11,27 +11,23 @@ formulario.addEventListener('submit' , function(){
 
     var tarea = document.querySelector("#izquierda").value;
     arrayTareas.push(tarea);
+    //Guardar en localStorage 
+    localStorage.setItem("tarea", JSON.stringify(arrayTareas));
     // console.log(arrayTareas);
 
     //Añdiendo tareas en el div de lista
     for(let tarea in arrayTareas){
         //Por cada iteracion del bucle se crea un parrafo
         parrafo = document.createElement("p");
-        texto = document.createTextNode(arrayTareas[tarea]);
-
-        //Funciona pero se esta sobreescribiendo
-        localStorage.setItem("tarea", arrayTareas[tarea]);
-        
+        texto = document.createTextNode(arrayTareas[tarea]);  
     }
-    parrafo.appendChild(texto);
-    //Guardar en localStorage y disponer al abrirse la pagina
-    document.querySelector("#lista").appendChild(parrafo);   
     
-    /*localStorage.setItem("parrafo", parrafo);
-    var memory = localStorage.getItem("parrafo");
-    var text = document.createTextNode(memory);
-    */
+    parrafo.appendChild(texto);
+    document.querySelector("#lista").appendChild(parrafo); 
    
 });
-
+//Mostrando elemento del localStorage al abrirse la pagina
+var recogiendo = localStorage.getItem("tarea");
+recogiendo = JSON.parse(recogiendo);
+document.querySelector("#lista").innerHTML = recogiendo; 
 
