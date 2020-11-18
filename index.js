8c1d6885eba2a1b33f256b7b82bcbda41f756056
Carlos <crodas035@gmail.com>
2020-11-18 16:33:05 +0100

    se muestran bien los datos devueltos de localstorage pero el localstorage se sobreescribe

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strict'
 
-
+//Variables
 var arrayTareas = [];
 var parrafo = "";
 var texto = "";
+var recogiendo = "";
+var parrafote = "";
+var textito = "";
+var lista;
+
+ //Mostrando elemento del localStorage al abrirse la pagina
+recogiendo = localStorage.getItem("tarea"); 
+
+//Si existen datos en el localstorage, cargalos y si no existen crea una lista
+if(recogiendo){
+     lista = JSON.parse(recogiendo);
+     LoadList(lista);
+}else{
+    lista = [];
+}
+
+function LoadList(array){
+    for (const tarea in array) {
+         parrafote = document.createElement("p");
+         textito = document.createTextNode(array[tarea]);
+         parrafote.appendChild(textito);
+         document.querySelector("#lista").appendChild(parrafote);
+    }
+}
 
 //Funcion recogiendo tarea introducida por usuario
 var formulario = document.querySelector("#formulario");
@@ -12,22 +36,18 @@ formulario.addEventListener('submit' , function(){
     var tarea = document.querySelector("#izquierda").value;
     arrayTareas.push(tarea);
     //Guardar en localStorage 
-    localStorage.setItem("tarea", JSON.stringify(arrayTareas));
-    // console.log(arrayTareas);
+    localStorage.setItem("tarea",JSON.stringify(arrayTareas)); 
 
     //Añdiendo tareas en el div de lista
-    for(let tarea in arrayTareas){
+    for(const task in arrayTareas){
         //Por cada iteracion del bucle se crea un parrafo
         parrafo = document.createElement("p");
-        texto = document.createTextNode(arrayTareas[tarea]);  
+        texto = document.createTextNode(arrayTareas[task]);
+        
     }
-    
     parrafo.appendChild(texto);
-    document.querySelector("#lista").appendChild(parrafo); 
-   
+    document.querySelector("#lista").appendChild(parrafo);
 });
-//Mostrando elemento del localStorage al abrirse la pagina
-var recogiendo = localStorage.getItem("tarea");
-recogiendo = JSON.parse(recogiendo);
-document.querySelector("#lista").innerHTML = recogiendo; 
+
+
 
